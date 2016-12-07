822faf02e3079d203732ab6ccc0a645de5c9b6eb
yowko.tsai <yowko.tsai@xuenn.com>
2016-12-07 16:40:20 +0800

    init

---
+++ TestOpensearch/Controllers/SearchController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestOpensearch.Controllers
+{
+    public class SearchController : Controller
+    {
+        // GET: Search
+        public ActionResult Index(string q="")
+        {
+            return Content(q);
+            //return View();
+        }
+    }
+}
